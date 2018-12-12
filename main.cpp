d5fdcd494fe410d3be5cbadb983a1c103acc6c05
rpavlov <rpavlov@mfisoft.ru>
2018-12-12 17:41:03 +0300

    create project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <exception>
+#include <unistd.h>
+#include <signal.h>
+#include <chrono>
+#include <string>
+#include <algorithm>
+#include <map>
+
+int main()
+{
+    printf("<<<<<<<<<<<<<<<<<<!!! check\n");
+             
+    return 0;
+}
